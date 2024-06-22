7f10a3cbbd646ebeebc5d4d6c2e443c22ebe0e54
wildstyl3r <wildstyl3r@yandex.ru>
2024-06-22 12:16:36 +0300

    Add test for correct Triangle creation

---
+++ Task1_CSharp/ShapeAreaLibrary.Tests/Triangle_Constructor.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using ShapeLibrary.Shapes;
+using System;
+
+namespace ShapeLibrary.Tests
+{
+    public class Triangle_Constructor
+    {
+        [Theory]
+        [InlineData(-1, 1, 1)]
+        [InlineData(1, -1, 1)]
+        [InlineData(1, 1, -1)]
+        public void Triangle_ConstructorTriangleInequalityViolation_ThrowsArgumentOutOfRangeException(double a, double b, double c)
+        {
+            var caughtException = Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(a, b, c));
+            Assert.Equal("Triangle inequality must be satisfied.", caughtException.Message);
+        }
+    }
+}
