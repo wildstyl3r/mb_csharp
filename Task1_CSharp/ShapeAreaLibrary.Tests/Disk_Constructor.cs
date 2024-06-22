06f0904330b25e0b988e96158b7fab37d92c4e6c
wildstyl3r <wildstyl3r@yandex.ru>
2024-06-22 12:16:48 +0300

    Add test for correct Disk creation

---
+++ Task1_CSharp/ShapeAreaLibrary.Tests/Disk_Constructor.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using ShapeLibrary.Shapes;
+using System;
+
+namespace ShapeLibrary.Tests
+{
+    public class Disk_Constructor
+    {
+
+        [Fact]
+        public void Disk_ConstructorWithNegativeRadius_ThrowsArgumentOutOfRangeException()
+        {
+            var caughtException = Assert.Throws<ArgumentOutOfRangeException>(() => new Disk(-1));
+            Assert.Equal("Radius must be non-negative.", caughtException.Message);
+        }
+    }
+}
