21fc031b8733d752810c0506ff98e539a9260bb9
wildstyl3r <wildstyl3r@yandex.ru>
2024-06-22 12:40:05 +0300

    Fix Triangle constructor test
    Change invalid arguments exception's type to ArgumentException

--- Task1_CSharp/ShapeAreaLibrary.Tests/Triangle_Constructor.cs
+++ Task1_CSharp/ShapeAreaLibrary.Tests/Triangle_Constructor.cs
@@ -10,9 +10,9 @@ namespace ShapeLibrary.Tests
         [InlineData(-1, 1, 1)]
         [InlineData(1, -1, 1)]
         [InlineData(1, 1, -1)]
-        public void Triangle_ConstructorTriangleInequalityViolation_ThrowsArgumentOutOfRangeException(double a, double b, double c)
+        public void Triangle_ConstructorTriangleInequalityViolation_ThrowsArgumentException(double a, double b, double c)
         {
-            var caughtException = Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(a, b, c));
+            var caughtException = Assert.Throws<ArgumentException>(() => new Triangle(a, b, c));
             Assert.Equal("Triangle inequality must be satisfied.", caughtException.Message);
         }
     }
